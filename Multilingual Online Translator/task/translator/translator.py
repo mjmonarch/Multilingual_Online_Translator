30c61ce09e411c389d4a2977821b154c517bbbf6
Sergii Kamenskyi <serg@apple.com>
2022-04-19 08:29:50 +0300

    Initial commit

---
+++ Multilingual Online Translator/task/translator/translator.py
@@ -0,0 +1,6 @@
+# --------------------------------------------- STAGE 1 ---------------------------------------------
+
+lang = input("Type \"en\" if you want to translate from French into English, or "
+             "\"fr\" if you want to translate from English into French:\n")
+word = input("Type the word you want to translate:\n")
+print("You choose \"{}\" as the language to translate \"{}\" to.".format(lang, word))
