c02d6745daadbaeddb7eb56695e7eeacb54c9640
Sergii Kamenskyi <serg@apple.com>
2022-04-20 20:14:07 +0300

    stage 6 completed!

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -163,37 +163,101 @@
 #     print("Something went wrong")
 
 # --------------------------------------------- STAGE 5 ---------------------------------------------
+# import requests
+# from bs4 import BeautifulSoup
+#
+# LANGUAGES = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch', 'Polish',
+#              'Portuguese', 'Romanian', 'Russian', 'Turkish']
+#
+# REVERSE_CONTEXT_URL = 'https://context.reverso.net/translation/'
+# translate_to_all_languages = False
+#
+# print("Hello, welcome to the translator. Translator supports:")
+# for i, lang in enumerate(LANGUAGES):
+#     print(f"{i + 1}. {lang}")
+# num = 0
+# while num not in range(1, 14):
+#     try:
+#         num = int(input("Type the number of your language:\n"))
+#     except ValueError:
+#         pass
+# first_lang = LANGUAGES[num - 1]
+# num = -1
+# while num not in range(0, 14):
+#     try:
+#         num = int(input("Type the number of a language you want to translate to "
+#                         "or '0' to translate to all languages:\n"))
+#     except ValueError:
+#         pass
+# if num == 0:
+#     translate_to_all_languages = True
+# else:
+#     second_lang = LANGUAGES[num - 1]
+# word = input("Type the word you want to translate:\n")
+#
+#
+# def get_translated_text(word, first_lang, second_lang, q):
+#     # creating request
+#     headers = {'User-Agent': 'Chrome/100.0'}
+#     url_adr = REVERSE_CONTEXT_URL + first_lang.lower() + '-' + second_lang.lower() + '/' + word
+#     try:
+#         r = requests.get(url_adr, headers=headers)
+#     except ValueError:
+#         print("Invalid resource!")
+#
+#     output = ''
+#     if r.status_code == 200:
+#         soup = BeautifulSoup(r.content, 'html.parser')
+#         translations_list = []
+#         examples_list = []
+#         for elem in soup.find('div', {'id': 'translations-content'}):
+#             text = elem.text.strip()
+#             if text:
+#                 translations_list.append(text)
+#         for elem in soup.find('section', {'id': 'examples-content'}).find_all('span', {'class': 'text'}):
+#             text = elem.text.strip()
+#             if text:
+#                 examples_list.append(text)
+#         output = f"{second_lang} Translations:\n"
+#         for i in range(q):
+#             output += translations_list[i] + '\n'
+#         output += '\n'
+#         output += f"{second_lang} Examples:\n"
+#         for i, example in enumerate(examples_list[:q*2]):
+#             output += example + '\n'
+#             if i % 2 == 1:
+#                 output += '\n'
+#     else:
+#         print("Something went wrong while parsing")
+#     return output
+#
+#
+# if not translate_to_all_languages:
+#     translated_text = get_translated_text(word, first_lang, second_lang, 5)
+# else:
+#     translated_text = ''
+#     for lang in LANGUAGES:
+#         if lang != first_lang:
+#             translated_text += get_translated_text(word, first_lang, lang, 1)
+#
+# file_name = word + ".txt"
+# with open(file_name, 'w', encoding='utf-8') as writer:
+#     writer.write(translated_text)
+# print(translated_text)
+
+# --------------------------------------------- STAGE 6 ---------------------------------------------
 import requests
 from bs4 import BeautifulSoup
+import argparse
+import copy
 
-LANGUAGES = ['Arabic', 'German', 'English', 'Spanish', 'French', 'Hebrew', 'Japanese', 'Dutch', 'Polish',
-             'Portuguese', 'Romanian', 'Russian', 'Turkish']
+LANGUAGES_FROM = ['arabic', 'german', 'english', 'spanish', 'french', 'hebrew', 'japanese', 'dutch', 'polish',
+             'portuguese', 'romanian', 'russian', 'turkish']
+LANGUAGES_TO = ['arabic', 'german', 'english', 'spanish', 'french', 'hebrew', 'japanese', 'dutch', 'polish',
+             'portuguese', 'romanian', 'russian', 'turkish', 'all']
 
 REVERSE_CONTEXT_URL = 'https://context.reverso.net/translation/'
 translate_to_all_languages = False
-
-print("Hello, welcome to the translator. Translator supports:")
-for i, lang in enumerate(LANGUAGES):
-    print(f"{i + 1}. {lang}")
-num = 0
-while num not in range(1, 14):
-    try:
-        num = int(input("Type the number of your language:\n"))
-    except ValueError:
-        pass
-first_lang = LANGUAGES[num - 1]
-num = -1
-while num not in range(0, 14):
-    try:
-        num = int(input("Type the number of a language you want to translate to "
-                        "or '0' to translate to all languages:\n"))
-    except ValueError:
-        pass
-if num == 0:
-    translate_to_all_languages = True
-else:
-    second_lang = LANGUAGES[num - 1]
-word = input("Type the word you want to translate:\n")
 
 
 def get_translated_text(word, first_lang, second_lang, q):
@@ -232,11 +296,24 @@ def get_translated_text(word, first_lang, second_lang, q):
     return output
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("lang_1", choices=LANGUAGES_FROM)
+parser.add_argument("lang_2", choices=LANGUAGES_TO)
+parser.add_argument("word")
+
+args = parser.parse_args()
+first_lang = args.lang_1
+if args.lang_2 == 'all':
+    translate_to_all_languages = True
+else:
+    second_lang = args.lang_2
+word = args.word
+
 if not translate_to_all_languages:
     translated_text = get_translated_text(word, first_lang, second_lang, 5)
 else:
     translated_text = ''
-    for lang in LANGUAGES:
+    for lang in LANGUAGES_FROM:
         if lang != first_lang:
             translated_text += get_translated_text(word, first_lang, lang, 1)
 
